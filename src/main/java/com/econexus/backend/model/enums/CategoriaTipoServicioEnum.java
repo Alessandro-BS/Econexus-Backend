eaf5e2faa7520ba138beacebaf0df6e2209e8f1f
Shayuri-GO <kiarago1103@gmail.com>
2026-05-19 22:54:49 -0500

    CategoriaTipoServicioEnum Categorias válidas de los servicios

---
+++ src/main/java/com/econexus/backend/model/enums/CategoriaTipoServicioEnum.java
@@ -0,0 +1,10 @@
+package com.econexus.backend.model.enums;
+
+public enum CategoriaTipoServicioEnum {
+    SOLIDO_PELIGROSO,
+    SOLIDO_NO_PELIGROSO,
+    LIQUIDO,
+    FUMIGACION,
+    DESINFECCION,
+    DESINSECTACION
+}
