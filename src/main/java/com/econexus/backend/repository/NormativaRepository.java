b38c94dc646c004c14888110f931c7b61e4b5e98
Shayuri-GO <kiarago1103@gmail.com>
2026-05-21 00:11:10 -0500

    NormativaRepository  Repositorio añadido

--- src/main/java/com/econexus/backend/repository/NormativaRepository.java
+++ src/main/java/com/econexus/backend/repository/NormativaRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface NormativaRepository extends JpaRepository<Normativa, Long> {
     List<Normativa> findAllByEstado(EstadoNormativaEnum estado);
+    boolean existsByCodigoIgnoreCase(String codigo);
 }
