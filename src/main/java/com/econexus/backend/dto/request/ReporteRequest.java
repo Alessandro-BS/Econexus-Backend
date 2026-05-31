82be4e77edeed963e63c3c8b35f3c71020033447
Franksua <U23229322@gmail.com>
2026-05-31 16:22:18 -0500

    valida que cantidad  de reportes se a mayor a cero

--- src/main/java/com/econexus/backend/dto/request/ReporteRequest.java
+++ src/main/java/com/econexus/backend/dto/request/ReporteRequest.java
@@ -1,5 +1,6 @@
 package com.econexus.backend.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,7 +23,10 @@ public class ReporteRequest {
 
     private Long ordenServicioId;
     private String descripcion;
+
+    @Min(value = 1, message = "La cantidad debe ser mayor a cero")
     private BigDecimal cantidad;
+    
     private String unidadMedida;         // KG | LITROS | M2 | UNIDAD
     private String estadoCumplimiento;   // PENDIENTE | EN_PROCESO | CUMPLIDO | OBSERVADO
 }
