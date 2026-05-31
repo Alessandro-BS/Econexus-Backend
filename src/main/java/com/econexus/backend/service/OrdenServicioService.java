8a9ec403f27cbe5444fa85db5ac40130df5f6c4e
Franksua <U23229322@gmail.com>
2026-05-31 18:32:49 -0500

    agrega metodos de filtrado por estado y cliente

--- src/main/java/com/econexus/backend/service/OrdenServicioService.java
+++ src/main/java/com/econexus/backend/service/OrdenServicioService.java
@@ -9,6 +9,9 @@ public interface OrdenServicioService {
 
     List<OrdenServicioResponse> listarOrdenes();
 
+    List<OrdenServicioResponse> filtrarPorEstadoPago(String estadoPago);
+    List<OrdenServicioResponse> filtrarPorCliente(Long clienteId);
+
     OrdenServicioResponse crearOrden(OrdenServicioRequest request);
 
     OrdenServicioResponse editarOrden(Long id, OrdenServicioRequest request);
