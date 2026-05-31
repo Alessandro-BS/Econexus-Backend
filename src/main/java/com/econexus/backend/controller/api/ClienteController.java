582c72fb0579139e8ec1f46eb638e14602c018b3
Alessandro-BS <fabrizio.bs9012@gmail.com>
2026-05-30 19:41:48 -0500

    Endpoint para editarCliente

--- src/main/java/com/econexus/backend/controller/api/ClienteController.java
+++ src/main/java/com/econexus/backend/controller/api/ClienteController.java
@@ -35,4 +35,12 @@ public class ClienteController {
         List<ClienteResponse> clientes = clienteService.buscarClientes(query);
         return ResponseEntity.ok(clientes);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteResponse> editarCliente(
+            @PathVariable Long id, 
+            @Valid @RequestBody ClienteRequest request) {
+        ClienteResponse clienteActualizado = clienteService.editarCliente(id, request);
+        return ResponseEntity.ok(clienteActualizado);
+    }
 }
